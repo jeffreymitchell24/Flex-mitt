eed62a86a67009c364f09a7904dfa74d82c7653b
jeffreysinteur <jeffreysinteur976@gmail.com>
2018-08-21 12:45:58 -0400

    comment disable dark screen

--- flex-mitt/AppDelegate.swift
+++ flex-mitt/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        UIApplication.shared.isIdleTimerDisabled = true // turn off screen sleep
+//        UIApplication.shared.isIdleTimerDisabled = true // turn off screen sleep
         
         Fabric.with([Crashlytics.self])
         return true
